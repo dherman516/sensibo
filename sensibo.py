f2600584dc9fbb231b98b2cd5e3473d74a6dd68b
DaveH <dherman516@gmail.com>
2020-07-27 13:17:03 +0300

    Update sensibo.py

--- sensibo.py
+++ sensibo.py
@@ -115,26 +115,24 @@ if __name__ == "__main__":
     outsideTemp = weather['current']['temp']
     fahrenheit = (outsideTemp * 9/5) + 32
 
-  # This code assumes units of Fahrenheit, MPH, and Relative Humidity by percentage.  In this example, a
-  # temperature of 35F, wind speed of 10mph, and relative humidity of 72% yields a "feels like" value of 27.4F
-  vTemperature = float(fahrenheit)
-  vRelativeHumidity = float(sensibohumidity)
+    # This code assumes units of Fahrenheit, MPH, and Relative Humidity by percentage.  In this example, a
+    # temperature of 35F, wind speed of 10mph, and relative humidity of 72% yields a "feels like" value of 27.4F
+    vTemperature = float(fahrenheit)
+    vRelativeHumidity = float(sensibohumidity)
 
  
-  # Replace it with the Heat Index, if necessary
-  if vFeelsLike == vTemperature and vTemperature >= 80:
-    vFeelsLike = 0.5 * (vTemperature + 61.0 + ((vTemperature-68.0)*1.2) + (vRelativeHumidity*0.094))
+    # Replace it with the Heat Index, if necessary
+    if vFeelsLike == vTemperature and vTemperature >= 80:
+      vFeelsLike = 0.5 * (vTemperature + 61.0 + ((vTemperature-68.0)*1.2) + (vRelativeHumidity*0.094))
  
-    if vFeelsLike >= 80:
-      vFeelsLike = -42.379 + 2.04901523*vTemperature + 10.14333127*vRelativeHumidity - .22475541*vTemperature*vRelativeHumidity - .00683783*vTemperature*vTemperature - .05481717*vRelativeHumidity*vRelativeHumidity + .00122874*vTemperature*vTemperature*vRelativeHumidity + .00085282*vTemperature*vRelativeHumidity*vRelativeHumidity - .00000199*vTemperature*vTemperature*vRelativeHumidity*vRelativeHumidity
-      if vRelativeHumidity < 13 and vTemperature >= 80 and vTemperature <= 112:
-        vFeelsLike = vFeelsLike - ((13-vRelativeHumidity)/4)*math.sqrt((17-math.fabs(vTemperature-95.))/17)
-        if vRelativeHumidity > 85 and vTemperature >= 80 and vTemperature <= 87:
-          vFeelsLike = vFeelsLike + ((vRelativeHumidity-85)/10) * ((87-vTemperature)/5)				
- 
-  
-  RealFeel = (vFeelsLike - 32) * 5/ 9
-  print "Feels like: " + '%0.1f' % (vFeelsLike) + "F"
+      if vFeelsLike >= 80:
+        vFeelsLike = -42.379 + 2.04901523*vTemperature + 10.14333127*vRelativeHumidity - .22475541*vTemperature*vRelativeHumidity - .00683783*vTemperature*vTemperature - .05481717*vRelativeHumidity*vRelativeHumidity + .00122874*vTemperature*vTemperature*vRelativeHumidity + .00085282*vTemperature*vRelativeHumidity*vRelativeHumidity - .00000199*vTemperature*vTemperature*vRelativeHumidity*vRelativeHumidity
+        if vRelativeHumidity < 13 and vTemperature >= 80 and vTemperature <= 112:
+          vFeelsLike = vFeelsLike - ((13-vRelativeHumidity)/4)*math.sqrt((17-math.fabs(vTemperature-95.))/17)
+          if vRelativeHumidity > 85 and vTemperature >= 80 and vTemperature <= 87:
+            vFeelsLike = vFeelsLike + ((vRelativeHumidity-85)/10) * ((87-vTemperature)/5)				  
+    RealFeel = (vFeelsLike - 32) * 5/ 9
+    print "Feels like: " + '%0.1f' % (vFeelsLike) + "F"
 
 
 
