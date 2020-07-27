69ae3d0d93423e4f1588037694cb527a8f1535b8
DaveH <dherman516@gmail.com>
2020-07-27 12:31:29 +0300

    Update getweath.py
    adding real feel for heat index...

--- getweath.py
+++ getweath.py
@@ -13,3 +13,30 @@ print s
 celsius = float(s)
 fahrenheit = (celsius * 9/5) + 32
 print('%.2f Celsius is: %0.2f Fahrenheit' %(celsius, fahrenheit))
+
+# I use a Python script to pull current weather conditions from the NOAA web service API. The NOAA web
+# service does not return a windchill value for all locations, but given temperature, relative humidity,
+# and wind speed you can calculate a “feels like” temperature as follows.
+
+# This code assumes units of Fahrenheit, MPH, and Relative Humidity by percentage.  In this example, a
+# temperature of 35F, wind speed of 10mph, and relative humidity of 72% yields a "feels like" value of 27.4F
+ 
+import math
+ 
+vTemperature = fahrenheit
+vRelativeHumidity = float(72)
+
+ 
+# Replace it with the Heat Index, if necessary
+if vFeelsLike == vTemperature and vTemperature >= 80:
+  vFeelsLike = 0.5 * (vTemperature + 61.0 + ((vTemperature-68.0)*1.2) + (vRelativeHumidity*0.094))
+ 
+  if vFeelsLike >= 80:
+    vFeelsLike = -42.379 + 2.04901523*vTemperature + 10.14333127*vRelativeHumidity - .22475541*vTemperature*vRelativeHumidity - .00683783*vTemperature*vTemperature - .05481717*vRelativeHumidity*vRelativeHumidity + .00122874*vTemperature*vTemperature*vRelativeHumidity + .00085282*vTemperature*vRelativeHumidity*vRelativeHumidity - .00000199*vTemperature*vTemperature*vRelativeHumidity*vRelativeHumidity
+    if vRelativeHumidity < 13 and vTemperature >= 80 and vTemperature <= 112:
+      vFeelsLike = vFeelsLike - ((13-vRelativeHumidity)/4)*math.sqrt((17-math.fabs(vTemperature-95.))/17)
+      if vRelativeHumidity > 85 and vTemperature >= 80 and vTemperature <= 87:
+        vFeelsLike = vFeelsLike + ((vRelativeHumidity-85)/10) * ((87-vTemperature)/5)				
+ 
+print "Feels like: " + '%0.1f' % (vFeelsLike) + "F"
+# Feels like: 27.4F
