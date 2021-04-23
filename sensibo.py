ce4f0224a0eb2e03cdee3859002ea86cbfc73a86
DaveH <dherman516@gmail.com>
2021-04-23 16:25:27 +0300

    Update sensibo.py

--- sensibo.py
+++ sensibo.py
@@ -138,12 +138,12 @@ if __name__ == "__main__":
 
     g.write ("{},{},{},{},{},{:.2f},{:.2f}\n".format(power,sensibotemp,sensibomode,fanlevel,targettemp,outsideTemp,RealFeel,sensibohumidity))
     f.write ("--------Temps---------\n")
-    f.write ("Target Temp: {}\n".format(targettemp))
+    f.write ("Target Temp: {} {}\n".format(targettemp, (targettemp * 9/5) + 32))
     f.write ("Sensibo Power On: {} Temp: {} Humidity: {:.2f} % RealFeel: {:.2f} State: {}  Fan: {}\n".format(power,sensibotemp,sensibohumidity,RealFeel,sensibomode,fanlevel))
     f.write ("Outside Temp: {}C /{}F\n".format(outsideTemp,fahrenheit))
 	
     print "--------Temps---------\n"
-    print "Target Temp: {}\n".format(targettemp)
+    print "Target Temp: {} {}\n".format(targettemp, (targettemp * 9/5) + 32)
     print "Sensibo Power On: {} Temp: {} Humidity: {:.2f} % RealFeel: {:.2f} State: {}  Fan: {}\n".format(power,sensibotemp,sensibohumidity,RealFeel,sensibomode,fanlevel)
     print "Outside Temp: {}C /{}F\n".format(outsideTemp,fahrenheit)
 	
